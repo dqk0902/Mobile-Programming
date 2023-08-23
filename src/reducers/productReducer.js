c38fbf41d5be93c6cd968287fff031d73f4b7c35
Duong Quoc Khang <dqk@DQKs-MacBook-Air.local>
2023-08-23 21:47:19 +0300

    Refactor structure of code

--- src/reducers/productReducer.js
+++ src/reducers/productReducer.js
@@ -1,3 +1,15 @@
+import axios from 'axios';
+
+export const fetchProducts = () => async (dispatch) => {
+  try {
+    const response = await axios.get('https://fakestoreapi.com/products');
+    dispatch({ type: 'FETCH_PRODUCTS_SUCCESS', payload: response.data });
+  } catch (error) {
+    dispatch({ type: 'FETCH_PRODUCTS_FAILURE', payload: error.message });
+  }
+};
+
+
 const initialState = {
     products: [],
     loading: false,
